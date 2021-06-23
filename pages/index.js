fae64437a63c33f85599d78761751841aea79f60
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-06-23 13:08:52 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+import products from "../products.json";
+
+import styles from "../styles/Home.module.css";
+import { fromImageToUrl } from "../utils/urls";
+import { twoDecimals } from "../utils/format";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {products.map((product) => (
+        <div key={product.name} className={styles.product}>
+          <Link href={`/products/${product.slug}`}>
+            <a>
+              <div className={styles.product__Rows}>
+                <div className={styles.product__ColImg}>
+                  <img src={fromImageToUrl(product.image)} />
+                </div>
+                <div className={styles.product__Col}>
+                  {product.name} ${twoDecimals(product.price)}
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
